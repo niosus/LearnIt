33ac4936809f1272f08cd4a7c92dd4cac4d979ac
niosus <zabugr@gmail.com>
2014-05-19 10:52:23 +0200

    fixed strings for 8 inch tablet

--- build/source/buildConfig/debug/com/learnit/LearnIt/BuildConfig.java
+++ build/source/buildConfig/debug/com/learnit/LearnIt/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String PACKAGE_NAME = "com.learnit.LearnIt";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 91;
+  public static final int VERSION_CODE = 92;
   public static final String VERSION_NAME = "";
 }
